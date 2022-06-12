ddcdb0ddb8c8089578ff76e82bdfd1050967ab6d
kphutiyagae <kopanophutiyagae@gmail.com>
2022-06-12 04:08:42 +0200

    mapped JSON data to UI component

--- assets/components/list-container/list-container.js
+++ assets/components/list-container/list-container.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Text } from 'react-native'
+import { View } from 'react-native';
+import ListItem from '../list-item/ListItem';
 
 const orderData = require('../../data/orders.json');    //Importing given order data
 
 export default function Listcontainer() {
   return (
-
-    <Text>Hello</Text>
+        orderData.map( (data, i) => ( 
+            <ListItem 
+            order_status={data.orderStatus}
+            pharmacy_name={data.pharmacyName}
+            order_total={data.orderTotal}
+            />
+         ))
   )
 }
--- assets/components/list-item/ListItem.js
+++ assets/components/list-item/ListItem.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { TextBase } from 'react-native'
-
-const orderData = require('../../data/orders.json');    //Importing given order data
+import { Text, View } from 'react-native'
 
 /*
 Functional component that will map JSON data to UI Component.
 */
 
-export default function ListItem() {
+export default function ListItem(props) {
     return (    
-    <div>
-
-    </div>
+    <View>
+        <Text>{props.order_status}</Text>
+        <Text>{props.pharmacy_name}</Text>
+        <Text>Sum Total : R{props.order_total}</Text>
+    </View>
   )
 }
